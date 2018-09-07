fd66ddb858218632dad262d658d7168a0da225cd
Chamath Silva <silva.chamath@corp.sysco.com>
2018-09-07 17:08:45 +0530

    update:build

--- es/CookieStorage.js
+++ es/CookieStorage.js
@@ -1,8 +1,11 @@
+import _classCallCheck from 'babel-runtime/helpers/classCallCheck';
+import _createClass from 'babel-runtime/helpers/createClass';
 
 import * as Cookies from 'js-cookie';
 
 /** @class */
-export default class CookieStorage {
+
+var CookieStorage = function () {
 
   /**
    * Constructs a new CookieStorage object
@@ -12,7 +15,9 @@ export default class CookieStorage {
    * @param {integer} data.expires Cookie expiration (in days, default: 365)
    * @param {boolean} data.secure Cookie secure flag (default: true)
    */
-  constructor(data) {
+  function CookieStorage(data) {
+    _classCallCheck(this, CookieStorage);
+
     this.domain = data.domain;
     if (data.path) {
       this.path = data.path;
@@ -37,49 +42,67 @@ export default class CookieStorage {
    * @param {object} value - the value
    * @returns {string} value that was set
    */
-  setItem(key, value) {
-    Cookies.set(key, value, {
-      path: this.path,
-      expires: this.expires,
-      domain: this.domain,
-      secure: this.secure
-    });
-    return Cookies.get(key);
-  }
 
-  /**
-   * This is used to get a specific key from storage
-   * @param {string} key - the key for the item
-   * This is used to clear the storage
-   * @returns {string} the data item
-   */
-  getItem(key) {
-    return Cookies.get(key);
-  }
 
-  /**
-   * This is used to remove an item from storage
-   * @param {string} key - the key being set
-   * @returns {string} value - value that was deleted
-   */
-  removeItem(key) {
-    return Cookies.remove(key, {
-      path: this.path,
-      domain: this.domain,
-      secure: this.secure
-    });
-  }
-
-  /**
-   * This is used to clear the storage
-   * @returns {string} nothing
-   */
-  clear() {
-    const cookies = Cookies.get();
-    let index;
-    for (index = 0; index < cookies.length; ++index) {
-      Cookies.remove(cookies[index]);
+  _createClass(CookieStorage, [{
+    key: 'setItem',
+    value: function setItem(key, value) {
+      Cookies.set(key, value, {
+        path: this.path,
+        expires: this.expires,
+        domain: this.domain,
+        secure: this.secure
+      });
+      return Cookies.get(key);
     }
-    return {};
-  }
-}
+
+    /**
+     * This is used to get a specific key from storage
+     * @param {string} key - the key for the item
+     * This is used to clear the storage
+     * @returns {string} the data item
+     */
+
+  }, {
+    key: 'getItem',
+    value: function getItem(key) {
+      return Cookies.get(key);
+    }
+
+    /**
+     * This is used to remove an item from storage
+     * @param {string} key - the key being set
+     * @returns {string} value - value that was deleted
+     */
+
+  }, {
+    key: 'removeItem',
+    value: function removeItem(key) {
+      return Cookies.remove(key, {
+        path: this.path,
+        domain: this.domain,
+        secure: this.secure
+      });
+    }
+
+    /**
+     * This is used to clear the storage
+     * @returns {string} nothing
+     */
+
+  }, {
+    key: 'clear',
+    value: function clear() {
+      var cookies = Cookies.get();
+      var index = void 0;
+      for (index = 0; index < cookies.length; ++index) {
+        Cookies.remove(cookies[index]);
+      }
+      return {};
+    }
+  }]);
+
+  return CookieStorage;
+}();
+
+export default CookieStorage;
--- es/DateHelper.js
+++ es/DateHelper.js
@@ -1,3 +1,5 @@
+import _classCallCheck from 'babel-runtime/helpers/classCallCheck';
+import _createClass from 'babel-runtime/helpers/createClass';
 /*!
  * Amazon Cognito Auth SDK for JavaScript
  * Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
@@ -14,41 +16,54 @@
  * License for the specific language governing permissions
  * and limitations under the License.
  */
-const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-const weekNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+var weekNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 /** @class */
-export default class DateHelper {
-  /**
-   * @returns {string} The current time in "ddd MMM D HH:mm:ss UTC YYYY" format.
-   */
-  getNowString() {
-    const now = new Date();
 
-    const weekDay = weekNames[now.getUTCDay()];
-    const month = monthNames[now.getUTCMonth()];
-    const day = now.getUTCDate();
-
-    let hours = now.getUTCHours();
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-
-    let minutes = now.getUTCMinutes();
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-
-    let seconds = now.getUTCSeconds();
-    if (seconds < 10) {
-      seconds = `0${seconds}`;
-    }
-
-    const year = now.getUTCFullYear();
-
-    // ddd MMM D HH:mm:ss UTC YYYY
-    const dateNow = `${weekDay} ${month} ${day} ${hours}:${minutes}:${seconds} UTC ${year}`;
-
-    return dateNow;
+var DateHelper = function () {
+  function DateHelper() {
+    _classCallCheck(this, DateHelper);
   }
-}
+
+  _createClass(DateHelper, [{
+    key: 'getNowString',
+
+    /**
+     * @returns {string} The current time in "ddd MMM D HH:mm:ss UTC YYYY" format.
+     */
+    value: function getNowString() {
+      var now = new Date();
+
+      var weekDay = weekNames[now.getUTCDay()];
+      var month = monthNames[now.getUTCMonth()];
+      var day = now.getUTCDate();
+
+      var hours = now.getUTCHours();
+      if (hours < 10) {
+        hours = '0' + hours;
+      }
+
+      var minutes = now.getUTCMinutes();
+      if (minutes < 10) {
+        minutes = '0' + minutes;
+      }
+
+      var seconds = now.getUTCSeconds();
+      if (seconds < 10) {
+        seconds = '0' + seconds;
+      }
+
+      var year = now.getUTCFullYear();
+
+      // ddd MMM D HH:mm:ss UTC YYYY
+      var dateNow = weekDay + ' ' + month + ' ' + day + ' ' + hours + ':' + minutes + ':' + seconds + ' UTC ' + year;
+
+      return dateNow;
+    }
+  }]);
+
+  return DateHelper;
+}();
+
+export default DateHelper;
